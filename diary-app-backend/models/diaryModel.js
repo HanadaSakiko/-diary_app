5978d73538cd2ec0e594d531db8f92d2013659e0
HanadaSakiko <sakiko.h1035@gmail.com>
2024-11-01 16:35:39 +0900

    add データをdiaryModelで処理する記述を追加

---
+++ diary-app-backend/models/diaryModel.js
@@ -0,0 +1,45 @@
+const db = require("../config/database")
+
+const DiaryModel = {
+
+    //日記のデータを取得する
+    // `callback` 関数には、エラー (`err`) と取得した結果（全日記のリスト） (`result`) が渡される
+    getDiaryList: (callback) => {
+      //すべての日記データを取得するクエリ
+      const sqlSelectDiaries = "SELECT id,title,content,DATE_FORMAT(date, '%Y-%m-%d') AS date FROM diaries"
+      //クエリを実行し、取得結果をコールバック関数に返す
+      db.query(sqlSelectDiaries, callback);
+    }
+
+    // 日記を作成する
+    addDiary:(req, callback) => {
+      const { title, content } = req.body;
+      //新たな日記データを挿入するクエリ
+      const sqlInsertDiary = "INSERT INTO diaries (title, content,date) VALUE(?, ?, NOW() )";
+      //クエリを実行し、取得結果をコールバック関数に返す
+      db.query(sqlInsertDiary, [title, content],callback)
+    }
+
+    // 特定の日記のみのデータを取得する
+  const getDiary = (req, callback) => {
+    const id = req.params.id; //URLパラメーターからidを取得
+    //フロント側からリクエストで送られたidに一致する日記を取得
+    const sqlSelectDiary = "SELECT id,title,content,DATE_FORMAT(date, '%Y-%m-%d') AS date FROM diaries WHERE id = ?"
+    db.query(sqlSelectDiary,[id],callback)
+  }
+
+  // 日記の更新
+  const updateDiary = (req, callback) => {
+    const id = req.params.id; //URLパラメーターからidを取得
+    const { title, content } = req.body;
+    const sqlUpdateDiary = "UPDATE diaries SET title = ?, content =? WHERE id = ?";
+    db.query(sqlUpdateDiary,[title, content, id], callback);
+  }
+
+  // 日記の削除
+  const deleteDiary = (req, callback) => {
+    const id = req.params.id; //URLパラメーターからidを取得
+    const sqlDeleteDiary = "DELETE from diaries WHERE id = ?";
+    db.query(sqlDeleteDiary,[id],callback);
+  }
+}
