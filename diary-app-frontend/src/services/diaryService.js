051b518684c332d3103550f0d500d58e8c899f7e
HanadaSakiko <sakiko.h1035@gmail.com>
2024-11-05 12:17:04 +0900

    fix Axiosの記述を修正apiの呼び出しのみに

--- diary-app-frontend/src/services/diaryService.js
+++ diary-app-frontend/src/services/diaryService.js
@@ -1,28 +1,28 @@
-import Axios from 'axios';
+import api from '../api/axiosInstance.js';
 
 //日記データを取得する
 const getDiaries = () => {
-  return Axios.get("http://localhost:3001/api/diaries")
+  return api.get("/diaries")
 }
 
 //日記を作成する
-  const addDiary = (title,content,) => {
-    return Axios.post("http://localhost:3001/api/diaries", { title, content })
-  }
+const addDiary = (title,content,) => {
+  return api.post("/diaries", { title, content })
+}
 
 //特定の日記データを取得する
 const getDiaryDetail = (id) => {
-  return Axios.get(`http://localhost:3001/api/diaries/${id}`);
+  return api.get(`/diaries/${id}`);
 }
 
 //特定の日記を更新する
 const updateDiary = (id,title,content) => {
-  return Axios.put(`http://localhost:3001/api/diaries/update/${id}`,{title, content});
+  return api.put(`/diaries/update/${id}`,{title, content});
 }
 
 //特定の日記データを削除する
 const deleteDiary = (id) => {
-  return Axios.delete(`http://localhost:3001/api/diaries/delete/${id}`);
+  return api.delete(`/diaries/delete/${id}`);
 }
 
 const diaryService = {
