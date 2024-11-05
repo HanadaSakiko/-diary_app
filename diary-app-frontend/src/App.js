0e96927ad4da8bb15ff1ab1ab630c909851e8cc9
HanadaSakiko <sakiko.h1035@gmail.com>
2024-11-05 10:34:50 +0900

    fix APP.jsファイルのインデント修正

--- diary-app-frontend/src/App.js
+++ diary-app-frontend/src/App.js
@@ -15,39 +15,39 @@ function App() {
 
   //日記の新規作成
   //タイトル、内容が空でないかを判定し、問題ない場合は日記を作成。そうでない場合はエラーとする
-const addDiary = (title, content) => {
-        if (title && content) {
-          diaryService.addDiary(title, content).then(() => {
-            alert("日記の作成に成功しました");
-            refreshDiaries(); //新たに作成されたデータを含む日記を反映
-            navigate("/diaries"); // 削除完了後に一覧画面に遷移
-          })
-            .catch(err => {
-              console.error("Error create diary: ", err);
-              alert("日記の新規作成に失敗しました");
-          })
-        } else {
-          alert("タイトルまたは内容を入力してください")
+  const addDiary = (title, content) => {
+    if (title && content) {
+      diaryService.addDiary(title, content).then(() => {
+        alert("日記の作成に成功しました");
+        refreshDiaries(); //新たに作成されたデータを含む日記を反映
+        navigate("/diaries"); // 削除完了後に一覧画面に遷移
+      })
+      .catch(err => {
+        console.error("Error create diary: ", err);
+        alert("日記の新規作成に失敗しました");
+      })
+    } else {
+      alert("タイトルまたは内容を入力してください")
+    }
   }
-}
 
-      //日記の削除処理
-      const deleteDiary = (id) => {
-        const result =  window.confirm("削除します。本当に宜しいですか？");
-        if (result) {
-          diaryService.deleteDiary(id).then(() => {
-            alert("指定された日記を削除しました");
-            refreshDiaries(); //削除後に最新のデータを取得
-            navigate("/diaries"); // 削除完了後に一覧画面に遷移
-          })
-            .catch(err => {
-              console.error("Error delete diary: ", err);
-              alert("指定された日記の削除に失敗しました");
-          })
-        }
-      }
+  //日記の削除処理
+  const deleteDiary = (id) => {
+    const result =  window.confirm("削除します。本当に宜しいですか？");
+    if (result) {
+      diaryService.deleteDiary(id).then(() => {
+        alert("指定された日記を削除しました");
+        refreshDiaries(); //削除後に最新のデータを取得
+        navigate("/diaries"); // 削除完了後に一覧画面に遷移
+      })
+      .catch(err => {
+        console.error("Error delete diary: ", err);
+        alert("指定された日記の削除に失敗しました");
+      })
+    }
+  }
 
-//画面表示領域(ルーティング設定)
+  //画面表示領域(ルーティング設定)
   return (
     <div>
       <Routes>
